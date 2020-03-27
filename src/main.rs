fa9afd5d90f7b909a978c5e7eb2d544816562bd3
Neben5 <jlsatpa@gmail.com>
2020-03-27 15:40:08 -0700

    added target, not finished

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     loop {}
-}
+}   
 
 /// This function is called on panic.
 #[panic_handler]
